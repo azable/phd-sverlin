cb3ad9c127f8680f5e39bbed349d4a979d3446ef
Alex Zable <alex.zable@unimelb.edu.au>
2026-06-06 05:11:31 +0000

    Separate value and operator distinction from linear node layer

--- compile/app/NodeBase.hs
+++ compile/app/NodeBase.hs
@@ -17,10 +17,8 @@ module NodeBase
 
     -- * Linear value handles
     VRef,
-    VPair (..),
 
     -- * Graph-building DSL
-    v,
     (.+.),
     (.*.),
     buildGraph,
@@ -36,8 +34,17 @@ import Prelude qualified as P
 
 type NId = Int
 
+data SomeNContent where
+  SomeNContent :: NContent tag -> SomeNContent
+
+instance P.Show SomeNContent where
+  show (SomeNContent content) = show content
+
+class KnownNTag tag where
+  matchContent :: SomeNContent -> Maybe (NContent tag)
+
 data N where
-  N :: NId -> NContent -> [NId] -> N
+  N :: NId -> SomeNContent -> [NId] -> N
 
 instance P.Show N where
   show (N nid content refs) =
@@ -48,18 +55,8 @@ instance P.Show N where
       padRight :: Int -> String -> String
       padRight n s = s P.++ P.replicate (n P.- P.length s) ' '
 
--- Internal/plain references used only inside stored graph nodes.
-newtype NRef = NRef NId
-  deriving (Show, Eq, Ord)
-
--- Linear public handle to a value node.
--- In a real module, export VRef abstractly; do not export VRef(..).
-data VRef where
-  VRef :: Ur NId %1 -> VRef
-
--- Linear public handle to an operator node.
-data ORef where
-  ORef :: Ur NId %1 -> ORef
+data NRef tag where
+  NRef :: Ur NId %1 -> NRef tag
 
 data BuilderState = BuilderState
   { nextId :: Ur NId,
@@ -80,74 +77,79 @@ instance Dupable BuilderState where
 
 type NBuilder = State BuilderState
 
-makeN :: NContent -> [NId] -> NBuilder (Ur NId)
+makeN :: NContent tag -> [NId] -> NBuilder (Ur NId)
 makeN content refs = do
   (BuilderState (Ur oldNextId) (Ur oldNodes)) <- get
   let newId = oldNextId
-      newNode = N newId content refs
+      newNode = N newId (SomeNContent content) refs
   put (BuilderState (Ur (newId + 1)) (Ur (oldNodes ++ [newNode])))
   return (Ur newId)
 
-lookupContent :: NId -> NBuilder (Ur NContent)
-lookupContent rid = do
-  (BuilderState (Ur _) (Ur nodes)) <- get
-  case find (\(N nid _ _) -> nid == rid) nodes of
-    Just (N _ content _) -> return (Ur content)
-    Nothing -> error $ "Node not found: " ++ show rid
+lookupRefContent :: (KnownNTag tag) => NRef tag %1 -> NBuilder (Ur (NId, NContent tag))
+lookupRefContent ref =
+  withNRef ref $ \rid -> do
+    BuilderState (Ur _) (Ur ns) <- get
+    case find (\(N nid _ _) -> nid == rid) ns of
+      Just (N _ someContent _) ->
+        case matchContent someContent of
+          Just content -> return (Ur (rid, content))
+          Nothing -> error "Internal type mismatch: ref tag does not match stored node content"
+      Nothing ->
+        error $ "Node not found: " P.++ P.show rid
 
-lookupContents :: [NId] -> NBuilder (Ur [NContent])
-lookupContents [] = return (Ur [])
-lookupContents (rid : rids) = do
-  (Ur c) <- lookupContent rid
-  (Ur cs) <- lookupContents rids
-  return (Ur (c : cs))
+makeNRef :: NContent tag -> [NId] -> NBuilder (NRef tag)
+makeNRef content refs = do
+  Ur nid <- makeN content refs
+  return (NRef (Ur nid))
 
-v :: Value -> NBuilder VRef
-v val = do
-  Ur nid <- makeN (V val) []
-  return (VRef (Ur nid))
-
-o :: Op -> NBuilder ORef
-o op = do
-  Ur nid <- makeN (O op) []
-  return (ORef (Ur nid))
-
--- Consume one value ref to expose its underlying node id.
-withVRef :: VRef %1 -> (NId -> NBuilder a) %1 -> NBuilder a
-withVRef (VRef (Ur nid)) k =
+withNRef :: NRef tag %1 -> (NId -> r) %1 -> r
+withNRef (NRef (Ur nid)) k =
   k nid
 
--- Consume one op ref to expose its underlying node id.
-withORef :: ORef %1 -> (NId -> NBuilder a) %1 -> NBuilder a
-withORef (ORef (Ur nid)) k =
-  k nid
+make1 :: NContent tag -> NBuilder (NRef tag)
+make1 content = makeNRef content []
 
--- This is your old `e`, but specialised to:
---   value, operator, value
---
--- The value references are consumed linearly.
-e :: VRef %1 -> ORef %1 -> VRef %1 -> NBuilder VRef
-e left op right =
-  withVRef left $ \leftId ->
-    withORef op $ \opId ->
-      withVRef right $ \rightId -> do
-        let refs = [leftId, opId, rightId]
-        (Ur contents) <- lookupContents refs
-        (Ur newId) <- makeN (eval contents) refs
-        return (VRef (Ur newId))
+combine3 ::
+  (KnownNTag a, KnownNTag b, KnownNTag c) =>
+  NRef a %1 ->
+  NRef b %1 ->
+  NRef c %1 ->
+  (NContent a -> NContent b -> NContent c -> NContent tag) ->
+  NBuilder (NRef tag)
+combine3 refA refB refC makeContent = do
+  Ur (aId, contentA) <- lookupRefContent refA
+  Ur (bId, contentB) <- lookupRefContent refB
+  Ur (cId, contentC) <- lookupRefContent refC
 
--- Explicit duplication / sharing.
---
--- This is the escape hatch you use when you intentionally want graph sharing.
-data VPair where
-  VPair :: VRef %1 -> VRef %1 -> VPair
+  makeNRef
+    (makeContent contentA contentB contentC)
+    [aId, bId, cId]
 
 -- DSL layer
 
-data NContent
-  = V Value
-  | O Op
-  deriving stock (P.Show)
+data KValue
+
+data KOp
+
+type VRef = NRef KValue
+
+type ORef = NRef KOp
+
+data NContent tag where
+  V :: Value -> NContent KValue
+  O :: Op -> NContent KOp
+
+instance P.Show (NContent tag) where
+  show (V val) = "V " P.++ P.show val
+  show (O op) = "O " P.++ P.show op
+
+instance KnownNTag KValue where
+  matchContent (SomeNContent c@(V _)) = Just c
+  matchContent _ = Nothing
+
+instance KnownNTag KOp where
+  matchContent (SomeNContent c@(O _)) = Just c
+  matchContent _ = Nothing
 
 data Value
   = I32 Int
@@ -159,14 +161,23 @@ data Op
   | Mul
   deriving stock (P.Show)
 
-eval :: [NContent] -> NContent
-eval [V (I32 x), O Add, V (I32 y)] = V (I32 (x + y))
-eval [V (I32 x), O Mul, V (I32 y)] = V (I32 (x * y))
-eval [V (F64 x), O Add, V (F64 y)] = V (F64 (x + y))
-eval [V (F64 x), O Mul, V (F64 y)] = V (F64 (x * y))
-eval contents = error $ "Type mismatch: " ++ displayContents
+eval :: NContent KValue -> NContent KOp -> NContent KValue -> NContent KValue
+eval (V (I32 x)) (O Add) (V (I32 y)) = V (I32 (x + y))
+eval (V (I32 x)) (O Mul) (V (I32 y)) = V (I32 (x * y))
+eval (V (F64 x)) (O Add) (V (F64 y)) = V (F64 (x + y))
+eval (V (F64 x)) (O Mul) (V (F64 y)) = V (F64 (x * y))
+eval lhs op rhs = error $ "Type mismatch" ++ displayContents
   where
-    displayContents = unwords $ P.map show contents
+    displayContents = "\n  LHS: " ++ P.show lhs ++ "\n  OP: " ++ P.show op ++ "\n  RHS: " ++ P.show rhs
+
+v :: Value -> NBuilder VRef
+v val = make1 (V val)
+
+o :: Op -> NBuilder ORef
+o op = make1 (O op)
+
+e :: VRef %1 -> ORef %1 -> VRef %1 -> NBuilder VRef
+e refA refOp refB = combine3 refA refOp refB eval
 
 (.+.) :: VRef %1 -> VRef %1 -> NBuilder VRef
 (.+.) a b = do
@@ -189,5 +200,7 @@ example = do
   n1 <- v (I32 42)
   n2 <- v (I32 100)
 
-  -- added <- n1 .+. n2
-  n1 .*. n2
+  n3 <- n1 .*. n2
+  n4 <- v (I32 10)
+
+  n3 .+. n4
