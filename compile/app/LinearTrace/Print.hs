c1e4d192660eefdf110695ff113d3ab72588e30c
Alex Zable <alex.zable@unimelb.edu.au>
2026-06-13 13:28:51 +0000

    Refactor print, improve readability

--- compile/app/LinearTrace/Print.hs
+++ compile/app/LinearTrace/Print.hs
@@ -11,6 +11,87 @@ module LinearTrace.Print
 import           LinearTrace.Core
 import           Prelude
 
+-- Layout constants
+eventIndexWidth :: Int
+eventIndexWidth = 3
+
+nodeListRefWidth :: Int
+nodeListRefWidth = 8
+
+snapshotRefWidth :: Int
+snapshotRefWidth = 6
+
+stepNameWidth :: Int
+stepNameWidth = 16
+
+stepIndent :: String
+stepIndent = "    "
+
+-- Colour/style constants
+eventTitleStyle :: [Ansi]
+eventTitleStyle = [AnsiBold]
+
+createColour :: Ansi
+createColour = Ansi256Fg 82 -- green
+
+observeColour :: Ansi
+observeColour = Ansi256Fg 51 -- cyan
+
+inspectColour :: Ansi
+inspectColour = Ansi256Fg 123 -- pale cyan
+
+useColour :: Ansi
+useColour = Ansi256Fg 220 -- yellow
+
+copyColour :: Ansi
+copyColour = Ansi256Fg 75 -- blue
+
+replaceColour :: Ansi
+replaceColour = Ansi256Fg 171 -- purple
+
+computeColour :: Ansi
+computeColour = Ansi256Fg 118 -- lime
+
+destroyColour :: Ansi
+destroyColour = Ansi256Fg 196 -- red
+
+sealColour :: Ansi
+sealColour = Ansi256Fg 37 --teal
+
+unsealColour :: Ansi
+unsealColour = Ansi256Fg 208 -- orange
+
+-- Step styles
+createStyle :: StepStyle
+createStyle = StepStyle "create" createColour
+
+observeStyle :: StepStyle
+observeStyle = StepStyle "observe" observeColour
+
+inspectStyle :: StepStyle
+inspectStyle = StepStyle "inspect" inspectColour
+
+useStyle :: StepStyle
+useStyle = StepStyle "use" useColour
+
+copyStyle :: StepStyle
+copyStyle = StepStyle "copy" copyColour
+
+replaceStyle :: StepStyle
+replaceStyle = StepStyle "replace" replaceColour
+
+computeStyle :: StepStyle
+computeStyle = StepStyle "compute" computeColour
+
+destroyStyle :: StepStyle
+destroyStyle = StepStyle "destroy" destroyColour
+
+sealStyle :: StepStyle
+sealStyle = StepStyle "seal" sealColour
+
+unsealStyle :: StepStyle
+unsealStyle = StepStyle "unseal" unsealColour
+
 class PrintEvent event where
   printEvent :: event acts -> String
 
@@ -22,95 +103,100 @@ printTrace graph = putStr (renderTrace graph)
 
 renderGraph :: (PrintEvent event) => TraceGraph event -> String
 renderGraph (TraceGraph nodes events) =
-  renderHeader "Graph"
-    ++ renderSummary nodes events
-    ++ "\n"
-    ++ renderNodes nodes
-    ++ "\n"
-    ++ renderEvents events
+  concat
+    [ renderHeader "Graph"
+    , renderSummary nodes events
+    , "\n"
+    , renderNodes nodes
+    , "\n"
+    , renderEvents events
+    ]
 
 renderTrace :: (PrintEvent event) => TraceGraph event -> String
 renderTrace (TraceGraph _ events) = renderEvents events
 
 renderSummary :: [NodeRecord] -> [TraceEvent event] -> String
 renderSummary nodes events =
-  "Nodes:  "
-    ++ show (length nodes)
-    ++ "\n"
-    ++ "Events: "
-    ++ show (length events)
-    ++ "\n"
+  concat
+    [ "Nodes:  "
+    , show (length nodes)
+    , "\n"
+    , "Events: "
+    , show (length events)
+    , "\n"
+    ]
 
 renderNodes :: [NodeRecord] -> String
 renderNodes nodes = renderHeader "Nodes" ++ concatMap renderNode nodes
 
 renderNode :: NodeRecord -> String
 renderNode (NodeRecord snapshot) =
-  "  "
-    ++ padRight 8 (renderNodeRefPlain (snapshotRef snapshot))
-    ++ renderSnapshotPayload snapshot
-    ++ "\n"
+  concat
+    [ "  "
+    , padRight nodeListRefWidth (renderNodeRefPlain (snapshotRef snapshot))
+    , renderSnapshotPayload snapshot
+    , "\n"
+    ]
 
 renderEvents :: (PrintEvent event) => [TraceEvent event] -> String
 renderEvents events =
-  renderHeader "Events"
-    ++ concat (zipWith renderEvent (enumFrom (0 :: Int)) events)
+  renderHeader "Events" ++ concat (zipWith renderEvent [0 :: Int ..] events)
 
 renderEvent :: (PrintEvent event) => Int -> TraceEvent event -> String
 renderEvent ix (TraceEvent event audit) =
-  padLeft 3 (show ix)
-    ++ " | "
-    ++ ansiBold
-    ++ printEvent event
-    ++ ansiReset
-    ++ "\n"
-    ++ renderAudit audit
-    ++ "\n"
+  concat
+    [ padLeft eventIndexWidth (show ix)
+    , " | "
+    , ansiText eventTitleStyle (printEvent event)
+    , "\n"
+    , renderAudit audit
+    , "\n"
+    ]
 
 renderAudit :: Audit acts -> String
 renderAudit EmptyAudit     = ""
 renderAudit (step :> rest) = renderAuditStep step ++ renderAudit rest
 
 renderAuditStep :: AuditStep act -> String
-renderAuditStep (CreateStep snapshot) =
-  renderSnapshotStep1 "create" ansiCreate snapshot
+renderAuditStep (CreateStep snapshot) = renderSnapshotStep1 createStyle snapshot
 renderAuditStep (ObserveStep snapshot) =
-  renderSnapshotStep1 "observe" ansiObserve snapshot
+  renderSnapshotStep1 observeStyle snapshot
 renderAuditStep (InspectStep snapshot) =
-  renderSnapshotStep1 "inspect" ansiInspect snapshot
-renderAuditStep (UseStep snapshot) = renderSnapshotStep1 "use" ansiUse snapshot
+  renderSnapshotStep1 inspectStyle snapshot
+renderAuditStep (UseStep snapshot) = renderSnapshotStep1 useStyle snapshot
 renderAuditStep (CopyStep original copy') =
-  renderSnapshotStep2 "copy" ansiCopy original copy'
-renderAuditStep (ReplaceStep old new) =
-  renderSnapshotStep2 "replace" ansiReplace old new
+  renderSnapshotStep2 copyStyle original copy'
+renderAuditStep (ReplaceStep old new) = renderSnapshotStep2 replaceStyle old new
 renderAuditStep (ComputeStep snapshot) =
-  renderSnapshotStep1 "compute" ansiCompute snapshot
+  renderSnapshotStep1 computeStyle snapshot
 renderAuditStep (DestroyStep snapshot) =
-  renderSnapshotStep1 "destroy" ansiDestroy snapshot
+  renderSnapshotStep1 destroyStyle snapshot
 renderAuditStep (SealStep owner child) =
-  renderSnapshotStep2 "seal" ansiSeal owner child
+  renderSnapshotStep2 sealStyle owner child
 renderAuditStep (UnsealStep owner child) =
-  renderSnapshotStep2 "unseal" ansiUnseal owner child
+  renderSnapshotStep2 unsealStyle owner child
 
-renderSnapshotStep1 :: String -> String -> NodeSnapshot tag -> String
-renderSnapshotStep1 name colour snapshot =
-  renderStepName name colour ++ " " ++ renderSnapshot snapshot ++ "\n"
+renderSnapshotStep1 :: StepStyle -> NodeSnapshot tag -> String
+renderSnapshotStep1 style snapshot =
+  concat [renderStepName style, " ", renderSnapshot snapshot, "\n"]
 
 renderSnapshotStep2 ::
-     String -> String -> NodeSnapshot first -> NodeSnapshot second -> String
-renderSnapshotStep2 name colour first second =
-  renderStepName name colour
-    ++ " "
-    ++ renderSnapshot first
-    ++ "\n"
-    ++ renderEmptyStepName
-    ++ " "
-    ++ renderSnapshot second
-    ++ "\n"
+     StepStyle -> NodeSnapshot first -> NodeSnapshot second -> String
+renderSnapshotStep2 style first second =
+  concat
+    [ renderStepName style
+    , " "
+    , renderSnapshot first
+    , "\n"
+    , renderEmptyStepName
+    , " "
+    , renderSnapshot second
+    , "\n"
+    ]
 
 renderSnapshot :: NodeSnapshot tag -> String
 renderSnapshot snapshot =
-  padRight 6 (renderNodeRef (snapshotRef snapshot))
+  padRight snapshotRefWidth (renderNodeRef (snapshotRef snapshot))
     ++ " "
     ++ renderSnapshotPayload snapshot
 
@@ -129,14 +215,15 @@ renderNodeRefPlain (NodeRef nodeId) = "N" ++ show nodeId
 renderPayloadView :: PayloadView -> String
 renderPayloadView (PayloadView text) = text
 
-renderStepName :: String -> String -> String
-renderStepName name colour = "    " ++ colourText colour (padLeft 16 name)
-
-renderEmptyStepName :: String
-renderEmptyStepName = "    " ++ padLeft 16 ""
-
 renderHeader :: String -> String
 renderHeader title = title ++ "\n" ++ replicate (length title) '-' ++ "\n"
+
+renderStepName :: StepStyle -> String
+renderStepName (StepStyle name colour) =
+  stepIndent ++ ansiText [colour] (padLeft stepNameWidth name)
+
+renderEmptyStepName :: String
+renderEmptyStepName = stepIndent ++ padLeft stepNameWidth ""
 
 padRight :: Int -> String -> String
 padRight n s = s ++ replicate (max 0 (n - length s)) ' '
@@ -144,44 +231,36 @@ padRight n s = s ++ replicate (max 0 (n - length s)) ' '
 padLeft :: Int -> String -> String
 padLeft n s = replicate (max 0 (n - length s)) ' ' ++ s
 
-colourText :: String -> String -> String
-colourText colour text = colour ++ text ++ ansiReset
+data StepStyle =
+  StepStyle String Ansi
 
-ansiReset :: String
-ansiReset = "\ESC[0m"
+data Ansi
+  = AnsiReset
+  | AnsiBold
+  | AnsiDim
+  | AnsiItalic
+  | AnsiUnderline
+  | AnsiFg Int
+  | AnsiBg Int
+  | Ansi256Fg Int
+  | Ansi256Bg Int
+  | AnsiRgbFg Int Int Int
+  | AnsiRgbBg Int Int Int
 
-ansiBold :: String
-ansiBold = "\ESC[1m"
+ansiText :: [Ansi] -> String -> String
+ansiText styles text = concatMap ansiCode styles ++ text ++ ansiCode AnsiReset
 
-ansi256Fg :: Int -> String
-ansi256Fg n = "\ESC[38;5;" ++ show n ++ "m"
-
-ansiCreate :: String
-ansiCreate = ansi256Fg 82 -- green
-
-ansiObserve :: String
-ansiObserve = ansi256Fg 51 -- cyan
-
-ansiInspect :: String
-ansiInspect = ansi256Fg 123 -- pale cyan
-
-ansiUse :: String
-ansiUse = ansi256Fg 220 -- yellow
-
-ansiCopy :: String
-ansiCopy = ansi256Fg 75 -- blue
-
-ansiReplace :: String
-ansiReplace = ansi256Fg 171 -- purple
-
-ansiCompute :: String
-ansiCompute = ansi256Fg 118 -- lime
-
-ansiDestroy :: String
-ansiDestroy = ansi256Fg 196 -- red
-
-ansiSeal :: String
-ansiSeal = ansi256Fg 213 -- pink
-
-ansiUnseal :: String
-ansiUnseal = ansi256Fg 208 -- orange
+ansiCode :: Ansi -> String
+ansiCode AnsiReset = "\ESC[0m"
+ansiCode AnsiBold = "\ESC[1m"
+ansiCode AnsiDim = "\ESC[2m"
+ansiCode AnsiItalic = "\ESC[3m"
+ansiCode AnsiUnderline = "\ESC[4m"
+ansiCode (AnsiFg n) = "\ESC[" ++ show n ++ "m"
+ansiCode (AnsiBg n) = "\ESC[" ++ show n ++ "m"
+ansiCode (Ansi256Fg n) = "\ESC[38;5;" ++ show n ++ "m"
+ansiCode (Ansi256Bg n) = "\ESC[48;5;" ++ show n ++ "m"
+ansiCode (AnsiRgbFg r g b) =
+  "\ESC[38;2;" ++ show r ++ ";" ++ show g ++ ";" ++ show b ++ "m"
+ansiCode (AnsiRgbBg r g b) =
+  "\ESC[48;2;" ++ show r ++ ";" ++ show g ++ ";" ++ show b ++ "m"
