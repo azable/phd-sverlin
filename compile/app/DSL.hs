53dfd6c58683d85a0a07564c6995d17e910a5a9e
Alex Zable <alex.zable@unimelb.edu.au>
2026-06-13 13:49:27 +0000

    Tweaks to DSL

--- compile/app/DSL.hs
+++ compile/app/DSL.hs
@@ -7,31 +7,57 @@
 {-# LANGUAGE RebindableSyntax      #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module DSL
-  ( -- Literals
+  ( -- * Public program types
+    Builder
+  , PrimitiveType(..)
+  , BinaryOp(..)
+  , Value
+  , Var
+  , Op
+  , VarNode
+  , IntNode
+  , DoubleNode
+  , IntVar
+  , DoubleVar
+  , -- * Literals
     int
   , double
-  , -- Operations
+  , literal
+  , -- * Variables
     declare
   , readVar
   , writeVar
   , discardVar
-  , eval
-  , literal
-  , operator
+  , -- * Values
+    discardValue
+  , -- * Operators
+    operator
+  , apply
   , (.+.)
   , (.*.)
-  , -- Runners/examples
+  , -- * Runners/examples
     run
   , example
   ) where
 
 import           Control.Functor.Linear hiding ((<$>), (<*>))
+import           Data.Proxy             (Proxy (..))
 import           LinearTrace
 import qualified Prelude                as P
 import           Prelude.Linear
 
+--------------------------------------------------------------------------------
+-- Configuration
+--------------------------------------------------------------------------------
+labelWidth :: Int
+labelWidth = 5
+
+--------------------------------------------------------------------------------
+-- DSL type vocabulary
+--------------------------------------------------------------------------------
 data PrimitiveType
   = TInt
   | TDouble
@@ -40,7 +66,6 @@ data BinaryOp
   = TAdd
   | TMul
 
--- Tags only. These are not value-level payload datatypes.
 data Value (ty :: PrimitiveType)
 
 data Var (ty :: PrimitiveType)
@@ -51,21 +76,31 @@ type instance Payload (Value 'TInt) = LInt (Value 'TInt)
 
 type instance Payload (Value 'TDouble) = LDouble (Value 'TDouble)
 
-type instance Payload (Op op lhs rhs out) = LUnit (Op op lhs rhs out)
-
 type instance Payload (Var ty) = LString (Var ty)
 
-int :: Int -> Payload (Value 'TInt)
-int = LInt
+type instance Payload (Op op lhs rhs out) = LUnit (Op op lhs rhs out)
 
-double :: Double -> Payload (Value 'TDouble)
-double = LDouble
+type Builder a = TraceBuilder Event a
 
+type IntNode = Node (Value 'TInt)
+
+type DoubleNode = Node (Value 'TDouble)
+
+type IntVar = VarNode 'TInt
+
+type DoubleVar = VarNode 'TDouble
+
+data VarNode ty where
+  VarNode :: Node (Var ty) %1 -> Slot (Var ty) (Value ty) %1 -> VarNode ty
+
+--------------------------------------------------------------------------------
+-- Event vocabulary
+--------------------------------------------------------------------------------
 data Event acts where
   Literal :: Event '[ Create (Value ty)]
   Operator :: Event '[ Create (Op op lhs rhs out)]
   DeclareVar
-    :: Event '[ Create (Var ty), Create (Value ty), Seal (Var ty) (Value ty)]
+    :: Event '[ Create (Value ty), Create (Var ty), Seal (Var ty) (Value ty)]
   ReadVar
     :: Event
          '[ Unseal (Var ty) (Value ty)
@@ -90,11 +125,27 @@ data Event acts where
          '[ Unseal (Var ty) (Value ty), Destroy (Var ty), Destroy (Value ty)]
   DiscardValue :: Event '[ Destroy (Value ty)]
 
-type Builder a = TraceBuilder Event a
+--------------------------------------------------------------------------------
+-- Literals
+--------------------------------------------------------------------------------
+int :: Int -> Payload (Value 'TInt)
+int = LInt
 
-data VarNode ty where
-  VarNode :: Node (Var ty) %1 -> Slot (Var ty) (Value ty) %1 -> VarNode ty
+double :: Double -> Payload (Value 'TDouble)
+double = LDouble
 
+literal ::
+     TracePayload (Value ty)
+  => Payload (Value ty)
+     %1 -> Builder (Node (Value ty))
+literal payload = do
+  Created node createValue <- create payload
+  Literal `explain` (createValue :~ Done)
+  return node
+
+--------------------------------------------------------------------------------
+-- Variables
+--------------------------------------------------------------------------------
 declare ::
      forall ty. TracePayload (Value ty)
   => String
@@ -104,7 +155,7 @@ declare name initial = do
   Created valueNode createValue <- create initial
   Created varNode createVar <- create (LString name :: Payload (Var ty))
   Sealed varNode' valueSlot sealValue <- seal varNode valueNode
-  DeclareVar `explain` (createVar :~ createValue :~ sealValue :~ Done)
+  DeclareVar `explain` (createValue :~ createVar :~ sealValue :~ Done)
   return (VarNode varNode' valueSlot)
 
 readVar ::
@@ -113,9 +164,9 @@ readVar ::
      %1 -> Builder (VarNode ty, Node (Value ty))
 readVar (VarNode var valueSlot) = do
   Unsealed var1 held unsealValue <- unseal var valueSlot
-  Copied held' copyNode copyHeld <- copy held
+  Copied held' copyNode copyValue <- copy held
   Sealed var2 valueSlot' sealValue <- seal var1 held'
-  ReadVar `explain` (unsealValue :~ copyHeld :~ sealValue :~ Done)
+  ReadVar `explain` (unsealValue :~ copyValue :~ sealValue :~ Done)
   return (VarNode var2 valueSlot', copyNode)
 
 writeVar ::
@@ -124,10 +175,10 @@ writeVar ::
      %1 -> Node (Value ty)
      %1 -> Builder (VarNode ty)
 writeVar (VarNode var valueSlot) newValue = do
-  Unsealed var1 oldHeld unsealValue <- unseal var valueSlot
-  Replaced newHeld replaceHeld <- replace oldHeld newValue
-  Sealed var2 valueSlot' sealValue <- seal var1 newHeld
-  WriteVar `explain` (unsealValue :~ replaceHeld :~ sealValue :~ Done)
+  Unsealed var1 oldValue unsealValue <- unseal var valueSlot
+  Replaced currentValue replaceValue <- replace oldValue newValue
+  Sealed var2 valueSlot' sealValue <- seal var1 currentValue
+  WriteVar `explain` (unsealValue :~ replaceValue :~ sealValue :~ Done)
   return (VarNode var2 valueSlot')
 
 discardVar :: TracePayload (Value ty) => VarNode ty %1 -> Builder ()
@@ -137,53 +188,17 @@ discardVar (VarNode var valueSlot) = do
   Destroyed destroyHeld <- destroy held
   DiscardVar `explain` (unsealValue :~ destroyVar :~ destroyHeld :~ Done)
 
-class ( TracePayload (Value lhs)
-      , TracePayload (Op op lhs rhs out)
-      , TracePayload (Value rhs)
-      , TracePayload (Value out)
-      ) =>
-      EvalOp op lhs rhs out
-  where
-  eval ::
-       Payload (Value lhs)
-       %1 -> Payload (Op op lhs rhs out)
-       %1 -> Payload (Value rhs)
-       %1 -> Payload (Value out)
+--------------------------------------------------------------------------------
+-- Values
+--------------------------------------------------------------------------------
+discardValue :: TracePayload (Value ty) => Node (Value ty) %1 -> Builder ()
+discardValue value = do
+  Destroyed destroyValue <- destroy value
+  DiscardValue `explain` (destroyValue :~ Done)
 
-instance EvalOp 'TAdd 'TInt 'TInt 'TInt where
-  eval (LInt x) LUnit (LInt y) = LInt (x + y)
-
-instance EvalOp 'TMul 'TInt 'TInt 'TInt where
-  eval (LInt x) LUnit (LInt y) = LInt (x * y)
-
-instance EvalOp 'TAdd 'TDouble 'TDouble 'TDouble where
-  eval (LDouble x) LUnit (LDouble y) = LDouble (x + y)
-
-instance EvalOp 'TMul 'TDouble 'TDouble 'TDouble where
-  eval (LDouble x) LUnit (LDouble y) = LDouble (x * y)
-
-e :: EvalOp op lhs rhs out
-  => Node (Value lhs)
-     %1 -> Node (Op op lhs rhs out)
-     %1 -> Node (Value rhs)
-     %1 -> Builder (Node (Value out))
-e lhsNode opNode rhsNode = do
-  Used lhs useLhs <- use lhsNode
-  Used op useOp <- use opNode
-  Used rhs useRhs <- use rhsNode
-  Computed outNode computeOut <- compute (eval <$> lhs <*> op <*> rhs)
-  Eval `explain` (useLhs :~ useOp :~ useRhs :~ computeOut :~ Done)
-  return outNode
-
-literal ::
-     TracePayload (Value ty)
-  => Payload (Value ty)
-     %1 -> Builder (Node (Value ty))
-literal payload = do
-  Created node createVal <- create payload
-  Literal `explain` (createVal :~ Done)
-  return node
-
+--------------------------------------------------------------------------------
+-- Operators
+--------------------------------------------------------------------------------
 operator ::
      TracePayload (Op op lhs rhs out)
   => Payload (Op op lhs rhs out)
@@ -193,24 +208,69 @@ operator payload = do
   Operator `explain` (createOp :~ Done)
   return node
 
+class ( TracePayload (Value lhs)
+      , TracePayload (Op op lhs rhs out)
+      , TracePayload (Value rhs)
+      , TracePayload (Value out)
+      ) =>
+      EvalOp op lhs rhs out
+  where
+  evalPayload ::
+       Payload (Value lhs)
+       %1 -> Payload (Op op lhs rhs out)
+       %1 -> Payload (Value rhs)
+       %1 -> Payload (Value out)
+
+instance EvalOp 'TAdd 'TInt 'TInt 'TInt where
+  evalPayload (LInt x) LUnit (LInt y) = LInt (x + y)
+
+instance EvalOp 'TMul 'TInt 'TInt 'TInt where
+  evalPayload (LInt x) LUnit (LInt y) = LInt (x * y)
+
+instance EvalOp 'TAdd 'TDouble 'TDouble 'TDouble where
+  evalPayload (LDouble x) LUnit (LDouble y) = LDouble (x + y)
+
+instance EvalOp 'TMul 'TDouble 'TDouble 'TDouble where
+  evalPayload (LDouble x) LUnit (LDouble y) = LDouble (x * y)
+
+apply ::
+     EvalOp op lhs rhs out
+  => Node (Value lhs)
+     %1 -> Node (Op op lhs rhs out)
+     %1 -> Node (Value rhs)
+     %1 -> Builder (Node (Value out))
+apply lhsNode opNode rhsNode = do
+  Used lhs useLhs <- use lhsNode
+  Used opPayload useOp <- use opNode
+  Used rhs useRhs <- use rhsNode
+  Computed outNode computeOut <-
+    compute (evalPayload <$> lhs <*> opPayload <*> rhs)
+  Eval `explain` (useLhs :~ useOp :~ useRhs :~ computeOut :~ Done)
+  return outNode
+
 (.+.) ::
-     Node (Value 'TInt)
-     %1 -> Node (Value 'TInt)
-     %1 -> Builder (Node (Value 'TInt))
+     forall ty. EvalOp 'TAdd ty ty ty
+  => Node (Value ty)
+     %1 -> Node (Value ty)
+     %1 -> Builder (Node (Value ty))
 (.+.) lhs rhs = do
-  add <- operator (LUnit :: Payload (Op 'TAdd 'TInt 'TInt 'TInt))
-  e lhs add rhs
+  add <- operator (LUnit :: Payload (Op 'TAdd ty ty ty))
+  apply lhs add rhs
 
 (.*.) ::
-     Node (Value 'TInt)
-     %1 -> Node (Value 'TInt)
-     %1 -> Builder (Node (Value 'TInt))
+     forall ty. EvalOp 'TMul ty ty ty
+  => Node (Value ty)
+     %1 -> Node (Value ty)
+     %1 -> Builder (Node (Value ty))
 (.*.) lhs rhs = do
-  mul <- operator (LUnit :: Payload (Op 'TMul 'TInt 'TInt 'TInt))
-  e lhs mul rhs
+  mul <- operator (LUnit :: Payload (Op 'TMul ty ty ty))
+  apply lhs mul rhs
 
+--------------------------------------------------------------------------------
+-- Example
+--------------------------------------------------------------------------------
 data FibState where
-  FibState :: VarNode 'TInt %1 -> VarNode 'TInt %1 -> FibState
+  FibState :: IntVar %1 -> IntVar %1 -> FibState
 
 iterateL :: Int -> (state %1 -> Builder state) -> state %1 -> Builder state
 iterateL n step state'
@@ -233,21 +293,39 @@ example :: Builder ()
 example = do
   a0 <- declare "a" (int 0)
   b0 <- declare "b" (int 1)
-  final <- iterateL 5 fibStep (FibState a0 b0)
-  case final of
-    FibState aN bN -> do
-      discardVar aN
-      discardVar bN
+  FibState aN bN <- iterateL 5 fibStep (FibState a0 b0)
+  discardVar aN
+  discardVar bN
 
 run :: Builder () -> TraceGraph Event
 run = buildGraph
 
--- Rendering logic
+--------------------------------------------------------------------------------
+-- Rendering
+--------------------------------------------------------------------------------
 padRight :: Int -> String -> String
 padRight n s = s P.++ P.replicate (P.max 0 (n P.- P.length s)) ' '
 
 padRightF :: String -> String
-padRightF = padRight 5
+padRightF = padRight labelWidth
+
+class PrimitiveLabel ty where
+  primitiveLabel :: Proxy ty -> String
+
+instance PrimitiveLabel 'TInt where
+  primitiveLabel _ = "I"
+
+instance PrimitiveLabel 'TDouble where
+  primitiveLabel _ = "D"
+
+class BinaryOpLabel op where
+  binaryOpLabel :: Proxy op -> String
+
+instance BinaryOpLabel 'TAdd where
+  binaryOpLabel _ = "Add"
+
+instance BinaryOpLabel 'TMul where
+  binaryOpLabel _ = "Mul"
 
 instance TracePayload (Value 'TInt) where
   payloadView _ (LInt i) = PayloadView (padRightF "Val" P.++ P.show i)
@@ -258,17 +336,19 @@ instance TracePayload (Value 'TDouble) where
 instance TracePayload (Var ty) where
   payloadView _ (LString name) = PayloadView (padRightF "Var" P.++ name)
 
-instance TracePayload (Op 'TAdd 'TInt 'TInt 'TInt) where
-  payloadView _ LUnit = PayloadView (padRightF "Op" P.++ "AddI")
-
-instance TracePayload (Op 'TMul 'TInt 'TInt 'TInt) where
-  payloadView _ LUnit = PayloadView (padRightF "Op" P.++ "MulI")
-
-instance TracePayload (Op 'TAdd 'TDouble 'TDouble 'TDouble) where
-  payloadView _ LUnit = PayloadView (padRightF "Op" P.++ "AddD")
-
-instance TracePayload (Op 'TMul 'TDouble 'TDouble 'TDouble) where
-  payloadView _ LUnit = PayloadView (padRightF "Op" P.++ "MulD")
+instance ( BinaryOpLabel op
+         , PrimitiveLabel lhs
+         , PrimitiveLabel rhs
+         , PrimitiveLabel out
+         ) =>
+         TracePayload (Op op lhs rhs out) where
+  payloadView _ LUnit =
+    PayloadView
+      (padRightF "Op"
+         P.++ binaryOpLabel (Proxy :: Proxy op)
+         P.++ primitiveLabel (Proxy :: Proxy lhs)
+         P.++ primitiveLabel (Proxy :: Proxy rhs)
+         P.++ primitiveLabel (Proxy :: Proxy out))
 
 instance PrintEvent Event where
   printEvent Literal      = "Literal"
