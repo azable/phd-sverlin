2072823c11d88af3193728124a7cb1177b3ac314
Alex Zable <alex.zable@unimelb.edu.au>
2026-06-14 10:59:55 +0000

    Add CSP solver base logic/state monad wrapper

--- compile/app/Solver.hs
+++ compile/app/Solver.hs
@@ -2,14 +2,22 @@
 
 module Solver where
 
+import           Control.Monad.State.Strict
 import           Numeric.Optimization.AD
+
+--------------------------------------------------------------------------------
+-- Variables and expressions
+--------------------------------------------------------------------------------
+newtype Var =
+  Var Int
+  deriving (Eq, Ord, Show)
 
 newtype Expr a = Expr
   { runExpr :: [a] -> a
   }
 
-var :: Int -> Expr a
-var i = Expr (!! i)
+valueOf :: Var -> Expr a
+valueOf (Var i) = Expr (!! i)
 
 sq :: Num a => a -> a
 sq x = x * x
@@ -43,18 +51,86 @@ instance Floating a => Floating (Expr a) where
   acosh (Expr f) = Expr $ acosh . f
   atanh (Expr f) = Expr $ atanh . f
 
-x :: Expr a
-x = var 0
+--------------------------------------------------------------------------------
+-- Problem builder
+--------------------------------------------------------------------------------
+data Term =
+  Term Rational (forall a. Floating a => Expr a)
 
-samplePolynomial :: Floating a => Expr a
-samplePolynomial = x ^ (2 :: Int) + 3 * x + 5
+data CSPState = CSPState
+  { nextVarId     :: Int
+  , initialValues :: [Double]
+  , energyTerms   :: [Term]
+  }
 
-energy :: Floating a => [a] -> a
-energy = runExpr samplePolynomial
+type BuildCSP = State CSPState
+
+emptyCSP :: CSPState
+emptyCSP = CSPState {nextVarId = 0, initialValues = [], energyTerms = []}
+
+newVar :: Double -> BuildCSP Var
+newVar initial = do
+  st <- get
+  let i = nextVarId st
+  put st {nextVarId = i + 1, initialValues = initialValues st ++ [initial]}
+  pure (Var i)
+
+addTerm :: Rational -> (forall a. Floating a => Expr a) -> BuildCSP ()
+addTerm weight expr = do
+  st <- get
+  put st {energyTerms = energyTerms st ++ [Term weight expr]}
+
+encourage :: (forall a. Floating a => Expr a) -> BuildCSP ()
+encourage = addTerm 1
+
+ensure :: (forall a. Floating a => Expr a) -> BuildCSP ()
+ensure = addTerm 100
+
+--------------------------------------------------------------------------------
+-- Compilation
+--------------------------------------------------------------------------------
+data CSP =
+  CSP [Double] (forall a. Floating a => [a] -> a)
+
+compile :: BuildCSP () -> CSP
+compile build = CSP initials energy
+  where
+    st = execState build emptyCSP
+    initials = initialValues st
+    terms = energyTerms st
+    energy xs =
+      sum [fromRational weight * runExpr expr xs | Term weight expr <- terms]
+
+solve :: CSP -> IO (Result [Double])
+solve (CSP initials energy) = minimize LBFGS def energy Nothing [] initials
+
+--------------------------------------------------------------------------------
+-- Constraint/objective helpers
+--------------------------------------------------------------------------------
+target :: Floating a => Var -> Rational -> Expr a
+target v wanted = sq (valueOf v - fromRational wanted)
+
+equal :: Floating a => Var -> Var -> Expr a
+equal a b = sq (valueOf a - valueOf b)
+
+sumEquals :: Floating a => Var -> Var -> Rational -> Expr a
+sumEquals a b wanted = sq (valueOf a + valueOf b - fromRational wanted)
+
+--------------------------------------------------------------------------------
+-- Example
+--------------------------------------------------------------------------------
+example :: BuildCSP ()
+example = do
+  x <- newVar 0
+  y <- newVar 0
+  encourage (target x 3)
+  encourage (target y 2)
+  ensure (sumEquals x y 6)
 
 test :: IO ()
 test = do
-  result <- minimize LBFGS def energy Nothing [] ([0] :: [Double])
+  let csp = compile example
+  result <- solve csp
   putStrLn "success:"
   print (resultSuccess result)
   putStrLn "solution:"
